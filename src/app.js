9cb28ab4498fcab91befc34fba21e41a978bc447
polarcit0 <solmartinez_98@hotmail.com>
2023-08-21 13:15:41 -0300

    html and css update, register creation and login

--- src/app.js
+++ src/app.js
@@ -12,3 +12,12 @@ app.listen(3001, () => {
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,"views/home.html"))
 })
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"views/register.html"));
+  });
+  
+  app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"views/login.html"));
+  });
+
